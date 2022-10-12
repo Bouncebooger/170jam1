ef460fc17757e5153cb537523efe2ba4c867f68a
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-10-12 15:23:34 -0700

    added enemy movement

---
+++ Assets/enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Transform target; // player position
+    public float speed;
+    public float inRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+        if (dist <= inRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        } else {
+            Vector3 tmp = this.transform.position;
+            tmp.y -= speed;
+            transform.position = tmp;
+        }
+    }
+}
