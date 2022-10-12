48da00898145ce09727e1ecf2608e5c21082b302
dwild <dwild@ucsc.edu>
2022-10-12 12:43:16 -0700

    spawn random thing works

---
+++ Assets/makenest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makenest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Nest;
+
+    private void Awake()
+    {
+        Vector3 randnewNest = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5),0);
+        Instantiate(Nest, randnewNest,Quaternion.identity);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
