67fef8502c5548053749f532ef3222370f8dbca7
dwild <dwild@ucsc.edu>
2022-10-12 14:29:09 -0700

    line render moment

---
+++ Assets/crawl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crawl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("linerenderpoint count"+ gameObject.positionCount);
+    }
+}
--- Assets/makeweb.cs
+++ Assets/makeweb.cs
@@ -6,16 +6,22 @@ public class makeweb : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject wire;
+   // private GameObject[] LineRenderer; 
     private int webcount;
     private Vector3 addy;
-     void Awake()
+    private LineRenderer webster;
+
+    void Awake()
     {
+
+
         addy = transform.position;
         Debug.Log("hello addy is " + addy);
     }
     void Start()
     {
-         
+        webster = GetComponent<LineRenderer>();
+
         webcount = 0;
     }
 
@@ -24,12 +30,17 @@ public class makeweb : MonoBehaviour
     {
         if (webcount == 0)
         {
+         
+            Vector3 point2 = new Vector3(-2f, 5, 10);
             Vector3 baddy = addy;
-            baddy.x = addy.x+0.5f;
+            baddy.x = addy.x+ (Random.Range(1,2));
             baddy.y = addy.y+0.5f;
             baddy.z = addy.z+0.5f;
-            
-            Instantiate(wire, baddy, Quaternion.identity);
+
+            webster.positionCount = 2;
+            webster.SetPosition(0,baddy);
+            webster.SetPosition(1,point2);
+            // Instantiate(wire, baddy, Quaternion.identity);
             webcount++;
             Debug.Log("baddy is " + baddy);
         }
