7859fa6b76e6cca50c988c77e54f634850ed55ce
dwild <dwild@ucsc.edu>
2022-10-12 19:04:16 -0700

    we are taking ls this time

--- Assets/crawl.cs
+++ Assets/crawl.cs
@@ -5,14 +5,58 @@ using UnityEngine;
 public class crawl : MonoBehaviour
 {
     // Start is called before the first frame update
+    private LineRenderer bolas;
+    private Vector3 startpoint;
+    public GameObject spread;
+    private Vector3 dirpoint;
+    private int spawned ;
+    
     void Start()
     {
-       
+        bolas = GetComponent<LineRenderer>();
+        startpoint = new Vector3(0, 0, 0);
+        Debug.Log(startpoint + "b4");
+        //  tempendpoint = startpoint 
+         dirpoint = startpoint;
+        if (gameObject.name == "North")
+        {
+            dirpoint.y += 1;
+        }
+        if (gameObject.name =="South")
+        {
+            dirpoint.y -= 1;
+        }
+        if (gameObject.name == "East")
+        {
+            dirpoint.x += 1;
+        }
+        if (gameObject.name == "West")
+        {
+            dirpoint.x -=1;
+        }
+        Debug.Log(dirpoint + "4tr");
+        bolas.positionCount=2;
+
+        bolas.SetPosition(0,startpoint);
+        bolas.SetPosition(1, dirpoint);
+        //  Instantiate(bruh,startpoint , Quaternion.identity);
+        spawned = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if(spawned == 0)
+        {
+              Instantiate(spread,startpoint , Quaternion.identity);
+            spawned++
+        }
+        
+
         //Debug.Log("linerenderpoint count"+ gameObject.positionCount);
     }
+
+  
+    
 }
--- Assets/makeweb.cs
+++ Assets/makeweb.cs
@@ -4,74 +4,43 @@ using UnityEngine;
 
 public class makeweb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject wire;
-    private bool[] webconnect = new bool[4] ;
-    private int webmade = 0;
-    private int baseconnect = 0;
-    private int webdistmax = 4;
-    private  LineRenderer[] amogus = new LineRenderer[4]; 
-    private int webcount;
-    private Vector3 addy;
-    private LineRenderer webster;
 
+
+    public GameObject webkin;
+    private Vector3 addy;
     void Awake()
     {
 
-        
+
         addy = transform.position;
         Debug.Log("hello addy is " + addy);
     }
     void Start()
     {
-        
-        
-        webster = GetComponent<LineRenderer>();
-        
-        webcount = 0;
-        webspawn();
+
+
+
+        //  webspawn();
+        // Debug.Log("Stop Posting about " + (amogus[webmade]);
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        
+
+
     }
 
-    void webspawn()
+   void webspawn(){
+        //instantiate 
+        }
+   void enemyspawn()
     {
-        
-        if (baseconnect < webmade )
-        {
-            Debug.Log("Stop Posting about SPIDERS(1982)2");
-            return;
-        }
-        else 
-        {
-            if (webmade >= 4)
-            {
-                Debug.Log("alarm!");
-                return;
-            }
-            int weblim = Random.Range(1, (4 - webmade));
-            for(  int z = 0; z < weblim; z++)
-            {
-                LineRenderer newweb = GetComponent<LineRenderer>();
-                amogus[webmade] = newweb;
-                webmade++;
-                Debug.Log("Stop Posting about SPIDERS(1982)"+weblim);
-            } 
-            //make wires numbering in range (1-(4-baseconnect))
-        }
+
     }
-    void webstep(LineRenderer orbweb)
-    {
-        if(orbweb.positionCount < webdistmax)
-        {
-            orbweb.positionCount++;
-      //      orbweb.SetPosition(orbweb.positionCount-1,)
-           //move towards base with wobble decreasing on every next point.
-        }
-    }
+   
+
+
 
 }
